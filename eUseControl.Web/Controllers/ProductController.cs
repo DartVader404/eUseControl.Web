0ad4c3593b5acd6181c0afbc29549faaeb8a0a18
BarganConstantin <chinchillemd@gmail.com>
2022-04-25 13:46:16 +0300

    some changes

--- eUseControl.Web/Controllers/ProductController.cs
+++ eUseControl.Web/Controllers/ProductController.cs
@@ -119,7 +119,7 @@ namespace eUseControl.Web.Controllers
             if (ModelState.IsValid)
             {
 
-                vAddToCartData data;
+                AddToCartData data;
                 var AddToCartResp = _product.AddToCartProduct(data);
 
                 if (AddToCartResp.Status)
