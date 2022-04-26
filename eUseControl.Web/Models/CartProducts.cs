6dffb8d940d0a1ed69f35c390ed135c7f3bf942e
BarganConstantin <chinchillemd@gmail.com>
2022-04-26 09:36:39 +0300

    remove unused field

--- eUseControl.Web/Models/CartProducts.cs
+++ eUseControl.Web/Models/CartProducts.cs
@@ -8,7 +8,6 @@ namespace eUseControl.Web.Models
     public class CartProducts
     {
         public int Id { get; set; }
-        public int ProductId { get; set; }
         public string ProductName { get; set; }
         public double Price { get; set; }
         public int Quantity { get; set; }
