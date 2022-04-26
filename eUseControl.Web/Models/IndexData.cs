51067a8635dfbc767746cf39b0565965cb540ce0
BarganConstantin <chinchillemd@gmail.com>
2022-04-26 07:51:09 +0300

    nothing

--- eUseControl.Web/Models/IndexData.cs
+++ eUseControl.Web/Models/IndexData.cs
@@ -11,7 +11,7 @@ namespace eUseControl.Web.Models
     {
         public string UserName { get; set; }
         public URole Level { get; set; }
-        public int CartProducts { get; set; }
+        public int CartProducts { get; set; }    //number of products in card to display in header
         public List<DbProduct> Products { get; set; }
         public int? SortBy { get; set; }
     }
