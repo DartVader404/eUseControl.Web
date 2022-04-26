dfaf89ae19af592d6ed4ed91656eb5507bc22081
BarganConstantin <chinchillemd@gmail.com>
2022-04-26 09:11:43 +0300

    new filed in model to find product by his id

--- eUseControl.Web/Models/CartProducts.cs
+++ eUseControl.Web/Models/CartProducts.cs
@@ -8,6 +8,7 @@ namespace eUseControl.Web.Models
     public class CartProducts
     {
         public int Id { get; set; }
+        public int ProductId { get; set; }
         public string ProductName { get; set; }
         public double Price { get; set; }
         public int Quantity { get; set; }
