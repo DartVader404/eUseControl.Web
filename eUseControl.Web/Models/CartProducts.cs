e462179ba32fd35430bd00f71f5af389e585b060
BarganConstantin <chinchillemd@gmail.com>
2022-04-26 07:45:41 +0300

    add product name field, neccesary for cart page

--- eUseControl.Web/Models/CartProducts.cs
+++ eUseControl.Web/Models/CartProducts.cs
@@ -8,12 +8,9 @@ namespace eUseControl.Web.Models
     public class CartProducts
     {
         public int Id { get; set; }
-
+        public string ProductName { get; set; }
         public double Price { get; set; }
-
         public int Quantity { get; set; }
-
         public string PreImgPath { get; set; }
-
     }
 }
